6acf5774eb9bd6e905321399c6e394d7d94f1a24
Younho Choo <yo+github@younho9.com>
2021-11-11 12:15:59 +0900

    ci: use narkdown packages

--- src/cli/utils/message.ts
+++
@@ -1,9 +0,0 @@
-import chalk from 'chalk';
-
-/**
- *  Display Validation Errors
- */
-export const flashError: (message: string) => never = (message: string) => {
-  console.error(chalk.bold.red(`✖ ${message}`));
-  return process.exit(1);
-};
